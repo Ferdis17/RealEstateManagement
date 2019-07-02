51d72ecfd93e4c063a549f08c1bce2f058d5e1f2
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-07-02 17:56:12 -0400

    first commit

---
+++ realEstate/src/domain/Admin.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Admin {
+	
+	  private String id;
+	    private String email;
+	    private String password;
+
+	    public static Admin authenticate(String email, String password) {
+	        if ( email.equals("admin@admin.com") && password.equals("admin") ) {
+	            return new Admin();
+	        }
+
+	        return null;
+	    }
+
+}
